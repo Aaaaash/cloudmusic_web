c827543de017577d9458b035542f98b704e34724
bxb <bxb@easub.com>
2017-02-02 23:44:21 +0800

    header

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -3,11 +3,21 @@
  */
 import React, { Component } from 'react';
 
+const inlineStyle = {
+  header: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#C62F2F',
+    display: 'flex',
+    alignItems: 'center'
+  },
+};
+
 export default class HomePage extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        header
+      <div style={inlineStyle.header}>
+        网易云音乐
       </div>
     );
   }
